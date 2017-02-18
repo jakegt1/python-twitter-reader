faafdf4501235748cfe3364a198a0ed7ddb085d3
Jake Torrance <Jaket1234@hotmail.com>
2017-02-18 14:57:24 +0000

    Added barebones of tweet templating

--- src/twitter_reader/App.py
+++ src/twitter_reader/App.py
@@ -2,10 +2,34 @@ from twitter_reader.Config import twitter
 from twitter_reader.Twitter import TwitterAPI
 from flask import Flask, render_template
 
+twitter_handler = TwitterAPI(
+    twitter["consumer_key"],
+    twitter["consumer_secret"]
+)
+
+def get_tweets_from_user_closure(twitter, access_token, access_secret):
+    def get_tweets_from_user(username):
+        return twitter.get_tweets_from_user(
+            username,
+            access_token,
+            access_secret
+        )
+    return get_tweets_from_user
+
+get_tweets_from_user = get_tweets_from_user_closure(
+    twitter_handler,
+    twitter["access_token"],
+    twitter["access_secret"]
+)
+
 app = Flask(__name__)
 @app.route("/")
 def index():
+    response_content = get_tweets_from_user('MaplecroftRisk')
+    statuses = response_content["statuses"]
+    statuses = [status["text"] for status in statuses]
     return render_template(
-        "index.html"
+        "index.html",
+        my_statuses = statuses
     )
 
